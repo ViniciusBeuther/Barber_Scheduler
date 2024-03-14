eb36da01a4b01e668392f39636ad841d6461e02c
EricBrisola <eric.coca13@gmail.com>
2024-03-14 12:07:00 -0300

    Refactor: changed the path of register image form

--- src/pages/RegisterScreen.jsx
+++ src/pages/RegisterScreen.jsx
@@ -1,30 +1,35 @@
-import { Button, Typography } from "@material-tailwind/react"
-import RegisterForm from "../components/RegisterForm"
-import registerIcon from "../../public/Icons/register_icon.png"
-import { Link } from "react-router-dom"
+import { Button, Typography } from "@material-tailwind/react";
+import RegisterForm from "../components/RegisterForm";
+import registerIcon from "../../src/assets/Icons/register_icon.png";
+import { Link } from "react-router-dom";
 import { IoArrowBackOutline } from "react-icons/io5";
 
 const RegisterScreen = () => {
-    return(
-        <article className="flex justify-center items-center pt-5 bg-customBlue-500 h-[100vh]">
-            <section className="w-full flex items-center justify-center bg-customBlue-500">
-                <span className="w-[50%]">
-                    <Link to={'/'}>
-                        <Button className="bg-customOrange-500 hover:bg-orange-700">
-                            <IoArrowBackOutline />
-                        </Button>
-                    </Link>
-                    <Typography variant="h3" color="white" className="mb-3 text-center">
-                        Registre-se abaixo
-                    </Typography>
-                    <RegisterForm />
-                </span>
-                <span className="flex items-center justify-center h-full ">
-                        <img src={registerIcon} width={360} height={360} alt="IconForRegisterPage" />
-                </span>
-            </section>
-        </article>
-    )
-}
+  return (
+    <article className="flex h-[100vh] items-center justify-center bg-customBlue-500 pt-5">
+      <section className="flex w-full items-center justify-center bg-customBlue-500">
+        <span className="w-[50%]">
+          <Link to={"/"}>
+            <Button className="bg-customOrange-500 hover:bg-orange-700">
+              <IoArrowBackOutline />
+            </Button>
+          </Link>
+          <Typography variant="h3" color="white" className="mb-3 text-center">
+            Registre-se abaixo
+          </Typography>
+          <RegisterForm />
+        </span>
+        <span className="flex h-full items-center justify-center ">
+          <img
+            src={registerIcon}
+            width={360}
+            height={360}
+            alt="IconForRegisterPage"
+          />
+        </span>
+      </section>
+    </article>
+  );
+};
 
-export default RegisterScreen
+export default RegisterScreen;
