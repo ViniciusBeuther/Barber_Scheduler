0284c961dcd79adf355298db15567319a66519f4
EricBrisola <eric.coca13@gmail.com>
2024-03-14 12:06:20 -0300

    Style: changed the form button color

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,18 +1,16 @@
 import { Button, Input, Typography, Avatar } from "@material-tailwind/react";
 const LoginForm = () => {
   return (
-    <section className=" flex h-screen items-center justify-center bg-blue-gray-50 ">
-      <article className=" h-[25rem] w-[20rem] rounded-md shadow-md flex flex-col items-center bg-white">
-        <div className="w-60 flex flex-col items-center h-[90%] justify-evenly">
+    <section className=" flex h-screen items-center justify-center bg-customBlue-500 ">
+      <article className=" flex h-[25rem] w-[20rem] flex-col items-center rounded-md bg-white shadow-md">
+        <div className="flex h-[90%] w-60 flex-col items-center justify-evenly">
           <Typography variant="h2" color="orange" className="font-normal">
             Login
           </Typography>
           <Avatar src="src\assets\user-image.png" alt="avatar" size="xl" />
           <Input size="md" label="E-mail" color="orange" type="email" />
           <Input size="md" label="Password" color="orange" type="password" />
-          <Button color="orange" className="w-60">
-            Conectar
-          </Button>
+          <Button className=" w-60 bg-customOrange-500">Conectar</Button>
         </div>
       </article>
     </section>
