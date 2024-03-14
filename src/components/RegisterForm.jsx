61a7ffa26726ecd434f4f5bb453c76a36b37a612
ViniciusBeuther <vinicius.beuther15@gmail.com>
2024-03-14 13:08:29 -0300

    WIP: form submit

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,41 +1,51 @@
 import { Button, Input, Option, Select, Textarea, Typography } from "@material-tailwind/react"
 
 const RegisterForm = () => {
+    
+    function handleSubmit(event){
+        event.preventDefault();
+        Object.entries(event.target).forEach((value) => {
+            console.log(value[1].value)
+        })
+    }
+
     return (
-        <div className="bg-customBlue-500 text-white flex flex-col gap-2">
-            <section className="flex gap-10 justify-between items-center">
-                <div className="w-full">
-                    <Input size="md" label="Nome da empresa" color="orange" className="w-full text-white" />
-                </div>
+        <div className="text-customBlue-500 flex flex-col gap-2">
+            <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+                <section className="bg-white flex gap-5 justify-between items-center">
+                    <div className="w-full">
+                        <Input size="md" label="Nome da empresa" color="orange" className="w-full text-customBlue-500" id="company__name" />
+                    </div>
 
-                <div className="w-full">
-                    <Input size="md" label="CNPJ" type="number" color="orange" className="w-full text-white" />
-                </div>
-            </section>
-            <section className="flex gap-10">
-                <div className="w-full">
-                    <Select label="Selecione o segmento" color="orange" className="text-white">
-                        <Option color="white">Saúde</Option>
-                        <Option color="white">Cuidados Pessoais</Option>
-                    </Select>
-                </div>
-            </section>
+                    <div className="w-full">
+                        <Input size="md" label="CNPJ" type="number" color="orange" className="w-full text-customBlue-500" id="company__cnpj" />
+                    </div>
+                </section>
+                <section className="flex gap-10">
+                    <div className="w-full">
+                        <Select label="Selecione o segmento" color="orange" className="text-customBlue-500" id="company__segment">
+                            <Option color="white">Saúde</Option>
+                            <Option color="white">Cuidados Pessoais</Option>
+                        </Select>
+                    </div>
+                </section>
 
-            <section>
-                <Textarea size="md" label="Descrição" color="orange" className="text-white" />
-            </section>
-            
-            <section>
-                <span className="flex flex-col gap-2">
-                    <Input size="md" label="E-mail" color="orange" className="text-white" />
-                    <Input size="md" label="Senha" type="password" color="orange" className="text-white" />
-                    <Typography variant="small" color="lightgray">
-                        *** Use no mínimo 8 caracteres, 1 letra maiuscula, 1 minuscula e 1 número
-                    </Typography>
-                </span>
-            </section>
+                <section>
+                    <Textarea size="md" label="Descrição" color="orange" className="text-customBlue-500" id="company__description" />
+                </section>
+                
+                <section>
+                    <span className="flex flex-col gap-2">
+                        <Input size="md" label="E-mail" color="orange" className="text-customBlue-500" />
+                        <Input size="md" label="Senha" type="password" color="orange" className="text-customBlue-500" id="company__password" />
+                        <Typography variant="small" color="gray">
+                            *** Use no mínimo 8 caracteres, 1 letra maiuscula, 1 minuscula e 1 número
+                        </Typography>
+                    </span>
+                </section>
 
-            <Button size="md" className="bg-customOrange-500 hover:bg-orange-700">Registrar</Button>
+                <Button type="submit" size="md" className="bg-customOrange-500 hover:bg-orange-700">Registrar</Button>
+            </form>
         </div>
     )
 }
--- src/pages/RegisterScreen.jsx
+++ src/pages/RegisterScreen.jsx
@@ -7,26 +7,19 @@ import { IoArrowBackOutline } from "react-icons/io5";
 const RegisterScreen = () => {
   return (
     <article className="flex h-[100vh] items-center justify-center bg-customBlue-500 pt-5">
-      <section className="flex w-full items-center justify-center bg-customBlue-500">
-        <span className="w-[50%]">
+      <section className="flex items-center bg-white rounded-md p-5 justify-center">
+        <span className="">
           <Link to={"/"}>
             <Button className="bg-customOrange-500 hover:bg-orange-700">
               <IoArrowBackOutline />
             </Button>
           </Link>
-          <Typography variant="h3" color="white" className="mb-3 text-center">
+          <Typography variant="h3" className="mb-3 text-center text-customBlue-500">
             Registre-se abaixo
           </Typography>
           <RegisterForm />
         </span>
-        <span className="flex h-full items-center justify-center ">
-          <img
-            src={registerIcon}
-            width={360}
-            height={360}
-            alt="IconForRegisterPage"
-          />
-        </span>
+        
       </section>
     </article>
   );
