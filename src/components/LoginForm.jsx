c164e35a5c965db488728151aaced2529b7aecd3
EricBrisola <eric.coca13@gmail.com>
2024-03-14 21:13:26 -0300

    Chore: added a state to track user inputs

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,16 +1,42 @@
 import { Button, Input, Typography, Avatar } from "@material-tailwind/react";
-const LoginForm = () => {
+import { Link } from "react-router-dom";
+
+const LoginForm = ({ onChange, onSubmit }) => {
   return (
     <section className=" flex h-screen items-center justify-center bg-customBlue-500 ">
-      <article className=" flex h-[25rem] w-[20rem] flex-col items-center rounded-md bg-white shadow-md">
+      <article className=" flex h-[25rem] w-[20rem] flex-col items-center rounded-md bg-white pt-2 shadow-md">
         <div className="flex h-[90%] w-60 flex-col items-center justify-evenly">
-          <Typography variant="h2" color="orange" className="font-normal">
+          <Typography variant="h2" className="font-normal text-customBlue-500">
             Login
           </Typography>
           <Avatar src="src\assets\user-image.png" alt="avatar" size="xl" />
-          <Input size="md" label="E-mail" color="orange" type="email" />
-          <Input size="md" label="Password" color="orange" type="password" />
-          <Button className=" w-60 bg-customOrange-500">Conectar</Button>
+          <form onSubmit={onSubmit} className="space-y-2">
+            <Input
+              size="md"
+              label="E-mail"
+              color="orange"
+              type="email"
+              name="email"
+              onChange={onChange}
+            />
+            <Input
+              size="md"
+              label="Password"
+              color="orange"
+              type="password"
+              name="password"
+              onChange={onChange}
+            />
+            <Button className=" w-60 bg-customOrange-500">Conectar</Button>
+          </form>
+          <Link to={"/register"}>
+            <Typography
+              variant="small"
+              className="font-normal text-customBlue-500"
+            >
+              Registre-se
+            </Typography>
+          </Link>
         </div>
       </article>
     </section>
