acddff131bdf2ebeb6d48bc504d8b8a65a5e77a8
EricBrisola <eric.coca13@gmail.com>
2024-03-15 00:03:57 -0300

    Refactor: changed user stats to login component

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,19 @@
 import { Button, Input, Typography, Avatar } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
 
-const LoginForm = ({ onChange, onSubmit }) => {
+const LoginForm = () => {
+  const [user, setUser] = useState({ email: "", password: "" });
+
+  const handleChange = (ev) => {
+    setUser({
+      ...user,
+      [ev.target.name]: ev.target.value,
+    });
+  };
+  console.log(user);
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+  };
   return (
     <section className=" flex h-screen items-center justify-center bg-customBlue-500 ">
       <article className=" flex h-[25rem] w-[20rem] flex-col items-center rounded-md bg-white pt-2 shadow-md">
@@ -10,14 +22,15 @@ const LoginForm = ({ onChange, onSubmit }) => {
             Login
           </Typography>
           <Avatar src="src\assets\user-image.png" alt="avatar" size="xl" />
-          <form onSubmit={onSubmit} className="space-y-2">
+          <form onSubmit={handleSubmit} className="space-y-2">
             <Input
               size="md"
               label="E-mail"
               color="orange"
               type="email"
               name="email"
-              onChange={onChange}
+              onChange={handleChange}
+              required
             />
             <Input
               size="md"
@@ -25,7 +38,8 @@ const LoginForm = ({ onChange, onSubmit }) => {
               color="orange"
               type="password"
               name="password"
-              onChange={onChange}
+              onChange={handleChange}
+              required
             />
             <Button className=" w-60 bg-customOrange-500">Conectar</Button>
           </form>
--- src/pages/LoginScreen.jsx
+++ src/pages/LoginScreen.jsx
@@ -2,21 +2,9 @@ import { useState } from "react";
 import LoginForm from "../components/LoginForm";
 
 const LoginScreen = () => {
-  const [user, setUser] = useState({ email: "", password: "" });
-
-  const handleChange = (ev) => {
-    setUser({
-      ...user,
-      [ev.target.name]: ev.target.value,
-    });
-  };
-  console.log(user);
-  const handleSubmit = (ev) => {
-    ev.preventDefault();
-  };
   return (
     <div>
-      <LoginForm onChange={handleChange} onSubmit={handleSubmit} />
+      <LoginForm />
     </div>
   );
 };
