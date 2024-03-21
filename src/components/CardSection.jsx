6848bdd6159dedd2dfed27c0b196d125c0a661d4
EricBrisola <eric.coca13@gmail.com>
2024-03-20 21:28:45 -0300

    refactor: created a component for each card

--- src/components/CardSection.jsx
+++ src/components/CardSection.jsx
@@ -3,109 +3,76 @@ import healthIcon from "../assets/Icons/health-icon.png";
 import foodServiceIcon from "../assets/Icons/food-service-icon.png";
 import hairSalon from "../assets/Icons/hair-salon-icon.png";
 import toolsIcon from "../assets/Icons/tools-icon.png";
-import { Button } from "@material-tailwind/react";
+import EstablishmentCard from "./EstablishmentCard";
 
 const CardSection = ({ chosenOptions }) => {
   const establishments = [
     {
+      id: 0,
       establishmentName: "Sessão filmes antigos",
       logoType: entertainmentIcon,
-      type: "entertainment",
+      category: "entertainment",
       summary: "Cinema com sucessos dos anos 90",
     },
     {
+      id: 1,
       establishmentName: "Clinica bem estar",
       logoType: healthIcon,
-      type: "health",
+      category: "health",
       summary: "Agende consultas com nossos psicólogos",
     },
     {
+      id: 2,
       establishmentName: "Paris 1",
       logoType: foodServiceIcon,
-      type: "food",
+      category: "food",
       summary: "Reserve uma mesa e venha provar nossos pratos",
     },
     {
+      id: 3,
       establishmentName: "Barbearia do calvo",
       logoType: hairSalon,
-      type: "aesthetic",
+      category: "aesthetic",
       summary: "Entre calvo e saia cabeludo",
     },
     {
+      id: 4,
       establishmentName: "Mecânica do Zeca",
       logoType: toolsIcon,
-      type: "autos",
+      category: "autos",
       summary: "Agende uma revisão",
     },
   ];
 
   const filteredEstablishments = establishments.filter((establishment) =>
     chosenOptions.some(
-      (option) => option.name === establishment.type && option.value,
+      (option) => option.name === establishment.category && option.value,
     ),
   );
 
   return (
     <section className="ml-10 flex flex-wrap gap-6">
       {filteredEstablishments.length < 1
-        ? establishments.map((establishment, idx) => {
+        ? establishments.map((establishment) => {
             return (
-              <article
-                key={idx}
-                className="flex h-40 w-60 flex-col justify-center gap-4 rounded-md bg-white p-2 shadow-md shadow-customBlue-500/5"
-              >
-                <div className="flex items-center gap-2">
-                  <div className="h-8 w-8 rounded-md bg-customBlue-500 p-1">
-                    <img
-                      src={establishment.logoType}
-                      alt={establishment.establishmentName}
-                    />
-                  </div>
-                  <p>{establishment.establishmentName}</p>
-                </div>
-                <p className="h-14">{establishment.summary}</p>
-                <div className="flex items-end justify-between">
-                  <p className="h-5 rounded-md bg-customBlue-300 p-1 text-xs leading-none text-white">
-                    {establishment.type}
-                  </p>
-                  <Button
-                    className="bg-customOrange-500 font-normal normal-case"
-                    size="sm"
-                  >
-                    Agendar
-                  </Button>
-                </div>
-              </article>
+              <EstablishmentCard
+                key={establishment.id}
+                establishmentName={establishment.establishmentName}
+                logoType={establishment.logoType}
+                category={establishment.category}
+                summary={establishment.summary}
+              />
             );
           })
-        : filteredEstablishments.map((establishment, idx) => {
+        : filteredEstablishments.map((establishment) => {
             return (
-              <article
-                key={idx}
-                className="flex h-40 w-60 flex-col justify-center gap-4 rounded-md bg-white p-2 shadow-md shadow-customBlue-500/5"
-              >
-                <div className="flex items-center gap-2">
-                  <div className="h-8 w-8 rounded-md bg-customBlue-500 p-1">
-                    <img
-                      src={establishment.logoType}
-                      alt={establishment.establishmentName}
-                    />
-                  </div>
-                  <p>{establishment.establishmentName}</p>
-                </div>
-                <p className="h-14">{establishment.summary}</p>
-                <div className="flex items-end justify-between">
-                  <p className="h-5 rounded-md bg-customBlue-300 p-1 text-xs leading-none text-white">
-                    {establishment.type}
-                  </p>
-                  <Button
-                    className="bg-customOrange-500 font-normal normal-case"
-                    size="sm"
-                  >
-                    Agendar
-                  </Button>
-                </div>
-              </article>
+              <EstablishmentCard
+                key={establishment.id}
+                establishmentName={establishment.establishmentName}
+                logoType={establishment.logoType}
+                category={establishment.category}
+                summary={establishment.summary}
+              />
             );
           })}
     </section>
