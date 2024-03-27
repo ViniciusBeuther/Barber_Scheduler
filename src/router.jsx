d6c7e63477b191baf32cb83ccd025efc33895d98
ViniciusBeuther <vinicius.beuther15@gmail.com>
2024-03-27 13:01:54 -0300

    New: Scheduled List

--- src/router.jsx
+++ src/router.jsx
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
         element: <HomeScreen />
     },
     {
-        path: "/admin/",
+        path: "/admin/:companyID",
         element: <AdminBoard />
     }
 ])
