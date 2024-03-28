5bb5c7032e8838b9293dbc598c9ad37680426243
ViniciusBeuther <vinicius.beuther15@gmail.com>
2024-03-28 13:03:33 -0300

    New: Schedule table DB connection

--- src/components/SchedulerBoard.jsx
+++ src/components/SchedulerBoard.jsx
@@ -1,11 +1,32 @@
 import { useParams } from "react-router-dom";
 import ModalChangeInfo from "./ModalChangeInfo";
 import { Checkbox, Typography } from "@material-tailwind/react";
+import { useEffect, useState } from "react";
+import { supabase } from "../API/CreateCompany";
 
 const SchedulerBoard = () => {
   const { companyID } = useParams();
+  const [data, setData] = useState();
+  
+  useEffect(() => {
+    async function fetchData(){
+        try{
+            let { data: Schedules, error } = await supabase
+            .from('Schedules')
+            .select("*")
+            setData(Schedules);
+            console.log("Fetched com sucessoo....")
+        } catch(error){
+            throw new Error("Algo deu errado")
+        }
+    }
+
+    fetchData();
+  }, [])
 
   return (
+    !data ? (<p>Carregando...</p>) : (
+
     <div>
       <section>
         <Typography
@@ -35,8 +56,16 @@ const SchedulerBoard = () => {
             className="h-5 w-5 rounded-full bg-white transition-all checked:border-green-600 checked:bg-green-500 hover:scale-105 hover:before:opacity-0"
           />
         </div>
+
+      </section>
+
+      <section>
+        <Typography>
+            {data[0].client_cpf}
+        </Typography>
       </section>
     </div>
+    )
   );
 };
 
